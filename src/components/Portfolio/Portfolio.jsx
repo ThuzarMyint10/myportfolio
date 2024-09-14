803960afffca75ab88c8a1c8d710f5495b3eb419
ThuzarMyint10 <thuzarmyint795@gmail.com>
2024-09-14 23:08:09 +0630

    changing images

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,51 +1,57 @@
+// import React, { useContext } from "react";
+// import "./Portfolio.css";
 import React, { useContext } from "react";
-import "./Portfolio.css";
-import { Swiper, SwiperSlide } from "swiper/react"
+import styles from "./Portfolio.module.css"; // Using CSS Modules
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
 import "swiper/css";
-import DetailDrawing1 from "../../img/detail_drawing1.jpg";
-import DetailDrawing2 from "../../img/detail_drawing2.jpg";
-import DetailDrawing3 from "../../img/detail_drawing3.jpg";
-import DetailDrawing4 from "../../img/detail_drawing4.jpg";
-import DetailDrawing5 from "../../img/detail_drawing5.jpg";
-import DetailDrawing6 from "../../img/detail_drawing6.jpg";
-import DetailDrawing7 from "../../img/detail_drawing7.jpg";
-import DetailDrawing8 from "../../img/detail_drawing8.jpg";
-import DetailDrawing9 from "../../img/detail_drawing9.jpg";
-import DetailDrawing10 from "../../img/detail_drawing10.jpg";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import { themeContext } from "../../Context";
+
+// Image URLs array
+const images = [
+  require("../../img/detail_drawing1.jpg"),
+  require("../../img/detail_drawing2.jpg"),
+  require("../../img/detail_drawing3.jpg"),
+  require("../../img/detail_drawing4.jpg"),
+  require("../../img/detail_drawing5.jpg"),
+  require("../../img/detail_drawing6.jpg"),
+  require("../../img/detail_drawing7.jpg"),
+  require("../../img/detail_drawing8.jpg"),
+  require("../../img/detail_drawing9.jpg"),
+  require("../../img/detail_drawing10.jpg"),
+];
+
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
+
   return (
-    <div className="portfolio" id="portfolio">
+    <div className={styles.portfolio} id="portfolio">
       {/* heading */}
-      <span style={{color: darkMode?'white': ''}}>Recent Projects</span>
+      <span style={{ color: darkMode ? "white" : "" }}>Recent Projects</span>
       <span>Portfolio</span>
 
-      {/* slider */}
+      {/* Swiper slider */}
       <Swiper
         spaceBetween={30}
         slidesPerView={3}
         grabCursor={true}
-        className="portfolio-slider"
+        pagination={{ clickable: true }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className={styles.portfolioSlider}
       >
-        {/*  </SwiperSlide>
-        <SwiperSlide> */}
-        <SwiperSlide>
-          <img src={DetailDrawing1} alt="" /> 
-          <img src={DetailDrawing2} alt="" /> 
-          <img src={DetailDrawing3} alt="" /> 
-          <img src={DetailDrawing4} alt="" /> 
-          <img src={DetailDrawing5} alt="" /> 
-          <img src={DetailDrawing6} alt="" /> 
-          <img src={DetailDrawing7} alt="" /> 
-          <img src={DetailDrawing8} alt="" /> 
-          <img src={DetailDrawing9} alt="" /> 
-          <img src={DetailDrawing10} alt="" /> 
-        </SwiperSlide>
+        {images.map((img, index) => (
+          <SwiperSlide key={index}>
+            <img src={img.default} alt={`Detail Drawing ${index + 1}`} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
 };
 
 export default Portfolio;
+
